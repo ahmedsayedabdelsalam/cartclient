81948dd3faff0e1a27cf1ddc3930887ca1b2f7cb
ahmed sayed abdelsalam <e.eng.ahmedsayed@gmail.com>
2019-10-23 00:28:43 +0200

    hide out of stock items

--- components/products/ProductVariation.vue
+++ components/products/ProductVariation.vue
@@ -5,9 +5,15 @@
       <div class="select is-fullwidth">
         <select :value="selectedVariationId" @change="changed($event, type)">
           <option value>Please choose</option>
-          <option v-for="variation in variations" :key="variation.id" :value="variation.id">
+          <option
+            :disabled="!variation.in_stock"
+            v-for="variation in variations"
+            :key="variation.id"
+            :value="variation.id"
+          >
             {{variation.name}}
             <template v-if="variation.price_varies">({{variation.price}})</template>
+            <template v-if="!variation.in_stock">(out of stock)</template>
           </option>
         </select>
       </div>
